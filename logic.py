4dbbe9dad9c7e3c524ff6f00f6cf29d1d55831b7
Jonty <jonty.morris@yahoo.co.nz>
2020-11-10 16:00:01 +1300

    Updated my example logic

--- logic.py
+++ logic.py
@@ -1,31 +1,40 @@
 import config
+import numpy as np
 
 # Example trading logic...
 # Update with your own indicators/rules.
 
-
 def get_latest_rsi(df):
-    periods = df[-14:]
+    np.seterr(divide='ignore', invalid='ignore')
 
-    # calculate the daily percent price changes
-    changes = periods[1:].Close.values - periods[0:13].Close.values
-    changes = changes / periods[0:13].Close.values * 100
+    try:
+        periods = df[-14:]
 
-    # seperate percent changes into gains and loses
-    gains = [change for change in changes if change >= 0]
-    loses = [change for change in changes if change < 0]
+        # calculate the daily percent price changes
+        changes = periods[1:].Close.values - periods[0:13].Close.values
+        changes = changes / periods[0:13].Close.values * 100
 
-    # average the gains and loses
-    average_gain = sum(gains) / len(gains)
-    average_loses = sum(gains) / len(loses)
+        # seperate percent changes into gains and loses
+        gains = [change for change in changes if change >= 0]
+        loses = [change for change in changes if change < 0]
 
-    # calculate RS and RSI
-    rs = average_gain / abs(average_loses)
-    rsi = 100 - (100 / (1 + rs))
+        # average the gains and loses
+        average_gain = sum(gains) / len(gains)
+        average_loses = sum(gains) / len(loses)
 
-    return rsi
+        # calculate RS and RSI
+        rs = average_gain / abs(average_loses)
+        rsi = 100 - (100 / (1 + rs))
+
+        return rsi
+    except Exception:
+        return 50 # default
 
 def should_buy(price, history):
+    # ignore penny stocks
+    if price < 1:
+        return False
+    
     # buy if RSI indicates oversold
     rsi = get_latest_rsi(history)
     return rsi <= 30
@@ -36,5 +45,8 @@ def should_sell(original_price, market_price):
     difference = market_price - original_price
     percent_change = (difference / original_price) * 100
 
-    # have we reached our profit percentage?
-    return percent_change >= config.sell_profit_percent
+    profit_margin = percent_change >= config.sell_profit_percent
+    loss_margin = percent_change <= config.sell_loss_percent
+
+    # sell if either of the margins are reached
+    return profit_margin or loss_margin
