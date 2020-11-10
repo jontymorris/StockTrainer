ac997b6bea41e40e96ffc534ccbfd757b064b4c1
Jonty <jonty.morris@yahoo.co.nz>
2020-11-10 16:00:27 +1300

    Added a working simulation

--- trainer.py
+++ trainer.py
@@ -2,6 +2,8 @@ import sharesies
 import yfinance
 import config
 import logic
+import pandas
+import os
 from datetime import datetime, timedelta
 
 
@@ -16,54 +18,115 @@ class PortfolioItem:
     def get_profit(self, current_price):
         new_amount = current_price * self.shares
         return new_amount - self.amount
+    
+    def get_amount(self, current_price):
+        new_amount = current_price * self.shares
+        return new_amount
 
 
-# get list of shares
-client = sharesies.Client()
-client = sharesies.Client()
-if client.login(config.username, config.password):
-    print('Connected to Sharesies')
-else:
-    print('Failed to login')
-    exit(-1)
+def get_shares_list():
+    client = sharesies.Client()
+    client = sharesies.Client()
+    if client.login(config.username, config.password):
+        print('> Connected to Sharesies')
+    else:
+        print('Failed to login')
+        exit(-1)
 
-companies = client.get_companies()
-print('> Retrieved list of shares')
+    return client.get_companies()
 
-# get their 5 year history
-histories = {}
-for company in companies:
-    symbol = company['code'] + '.NZ'
-    stock = yfinance.Ticker(symbol)
+def get_histories(shares):
+    # get their 5 year history
+    histories = {}
+    for company in shares:
+        symbol = company['code'] + '.NZ'
+        cached_path = f'./histories/{symbol}.csv'
 
-    history = stock.history(period='5y', interval='1d')
-    history = history.groupby(history.index.date, group_keys=False)
+        if os.path.exists(cached_path):
+            history = pandas.read_csv(cached_path)
+            history['Date'] = pandas.to_datetime(history['Date'])
+            history = history.set_index('Date')
+        else:
+            stock = yfinance.Ticker(symbol)
+            history = stock.history(period='5y', interval='1d')
+            history.to_csv(cached_path)
 
-    histories[company['code']] = history
+        histories[company['code']] = history
 
-# give yourself $1000
-balance = 1000
-portfolio = []
-print('> Mock portfolio created')
+    return histories
 
-# go from 2017 to 2018 in daily intervals
-start_date = datetime(year=2017, month=1, day=1)
-end_date = datetime(year=2018, month=1, day=1)
-interval = timedelta(days=1)
-print('> Running simulation...')
+def analyze_stock(symbol, history, portfolio, balance, keep_buying):
+    transaction_value = 0
+    
+    # try to lookup the portfolio record
+    item = next(
+        (item for item in portfolio if item.symbol == symbol),
+        False
+    )
 
-current_date = start_date
-while current_date < end_date:
-    # todo: get history up till current date
+    try:
+        # try to lookup the market price 
+        market_price = history[-1:].Close.values[0]
+    except:
+        return transaction_value
 
-    # loop through each of the stocks
-        # check if it should be bought
-            # add to bought stocks
-            # reduce from balance
-    # loop through inventory
-        # check if it should be sold
+    # we own this stock and should sell
+    if item and logic.should_sell(item.price, market_price):
+        print(f'# Selling {item.symbol} for ${market_price}')
+        transaction_value = item.get_amount(market_price)
+        portfolio.remove(item)
+    
+    # we don't own and should buy
+    if not item and logic.should_buy(market_price, history) and keep_buying:
+        quantity = 100
+        cost = market_price * quantity
+        if balance >= cost:
+            print(f'# Buying {symbol} for ${market_price}')
+            portfolio.append(PortfolioItem(symbol, market_price, quantity))
+            transaction_value -= cost
+    
+    return transaction_value
 
-    pass
+def perform_simulation(histories):
+    # give yourself $10,000
+    inital = 10000
+    balance = inital
+    portfolio = []
+    print('> Mock portfolio created')
 
-print(f'\nNew balance: ${balance}')
-print(f'Total returns: {(balance - 1000) / 10}%')
+    # go from 2017 to 2018 in daily intervals
+    start_date = datetime(year=2017, month=1, day=1)
+    end_date = datetime(year=2018, month=1, day=1)
+    interval = timedelta(days=1)
+    print('> Running simulation...')
+
+    # keep looping until past end date and sold everything
+    current_date = start_date
+    while current_date < end_date or len(portfolio) > 0:
+        keep_buying = current_date <= end_date
+
+        # go through each stock daily
+        for symbol in histories:
+            # get history up till current date
+            history = histories[symbol]
+            history = history[history.index <= current_date]
+
+            balance += analyze_stock(symbol, history,
+                portfolio, balance, keep_buying)
+
+        current_date += interval
+
+    returns = (balance - inital) / inital * 100
+
+    print(f'\nNew balance: ${round(balance, 3)}')
+    print(f'Total returns: {round(returns, 3)}%')
+
+def main():
+    shares = get_shares_list()
+    histories = get_histories(shares)
+
+    perform_simulation(histories)
+
+
+if __name__ == '__main__':
+    main()
